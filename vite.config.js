2b00693ea68bdaec6155fe72d8a898ec40caec62
Dondonn <sd12er12@gmail.com>
2023-11-10 19:41:34 +0100

    fix white website

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import postcss from './postcss.config.js'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'url'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Set base path to '/'
   define: {
     'process.env': process.env
   },
@@ -20,28 +19,11 @@ export default defineConfig({
           return val.replace(/^~/, "");
         },
       },
-      // Fix for using __dirname in Vite
-      {
-        find: /^@\//,
-        replacement: fileURLToPath(new URL('./src/', import.meta.url)),
-      },
     ],
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
     commonjsOptions: {
       transformMixedEsModules: true,
-    },
-    rollupOptions: {
-      output: {
-        assetFileNames: assetInfo => {
-          if (assetInfo.name === 'index.html') {
-            return 'index.html';
-          }
-          return `assets/${assetInfo.name}`;
-        }
-      }
     }
-  },
+  }
 })
