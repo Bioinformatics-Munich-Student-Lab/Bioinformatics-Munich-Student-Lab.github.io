be93407a9a311182f53a332632a4e75716ab34fc
Dondonn <sd12er12@gmail.com>
2023-11-12 04:31:25 +0100

    fix white website

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dev&', // work
+  base: '/dev/', // work
   define: {
     'process.env': process.env
   },
